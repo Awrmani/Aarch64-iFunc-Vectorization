f2d1b517be8dfcae6c7f2b4b1c2f33e584d13d64
Arman Valaee <armanvalaee@gmail.com>
2022-12-14 21:20:41 -0500

    Final modifications

--- function.h
+++ function.h
@@ -1,13 +1,8 @@
 #ifndef FUNCTION_H
 #define FUNCTION_H
 
-#ifndef FUNCTION_H
-#define FUNCTION_H
-
 /* This file was automatically generated.  Do not edit! */
 void adjust_channels(unsigned char *image,int x_size,int y_size,float red_factor,float green_factor,float blue_factor);
 
-
-#endif
 
 #endif
